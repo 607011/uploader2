de56788de811b29b2ca5eaf2552c2b0639f82486
Oliver Lau <oliver@von-und-fuer-lau.de>
2012-08-22 07:28:20 +0200

    deploy.sh verbessert.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,34 @@
 #!/bin/sh
-./bin/yuicompress.sh
+echo
+echo ==============================================
+echo DEPLOYING
+echo ==============================================
+mkdir -p deploy/js
+mkdir -p deploy/css
+mkdir -p deploy/img
+
+cp img/* deploy/img
+
+PATHTOYUI=`pwd`/bin
+echo ==============================================
+echo COMPRESSING JAVASCRIPTS
+echo ==============================================
+cd js/src
+for f in *.js
+do
+  java -jar ${PATHTOYUI}/yuicompressor-2.4.2.jar -v --type js --charset utf-8 -o ../../deploy/js/$f $f
+done
+cd ../..
+echo ==============================================
+echo COMPRESSING STYLESHEETS
+echo ==============================================
+cd css/src
+for f in *.css
+do
+  java -jar ${PATHTOYUI}/yuicompressor-2.4.2.jar -v --type css --charset utf-8 -o ../../deploy/css/$f $f
+done
+cd ../..
+sed -e "s/js\/src/js/" -e "s/css\/src/css/" index.html > deploy/index.html
+
+
+echo READY.
